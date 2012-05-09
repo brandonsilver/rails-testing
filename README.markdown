06f2a903257472a8e542a99e1a43ec1ade551297
brandonsilver <brandon@silverimaging.net>
2012-05-09 14:18:53 -0500

    first commit, hitting the ground running

---
+++ README.markdown
@@ -0,0 +1,1 @@
+This will be _the_ place for my basic adventures in Ruby on Rails.
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '188f8b9b06267bf4b75d87e666fff63a8f64806a681d0d0839366ec4496fb12926dc71e7c5a8a26ca964d3cb3d89b64e0a5dfa798f523c6c26f2fd012b692749'
